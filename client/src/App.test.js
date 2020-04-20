c4d4a4d72f185305fd71c3e610c300335b4b9580
CWC-88 <29718763+CWC-88@users.noreply.github.com>
2020-04-19 22:09:26 -0500

    sdafas

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { render , waitForDomChange  } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
+// import "./App.css";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -37,3 +38,34 @@ test('are the numbers there', async () => {
 
 })
 
+
+test('is name container shown ', async () => {
+  const {baseElement} = render(<App/>)
+  await waitForDomChange();
+const cardName = baseElement.querySelector('.playnameactual')
+
+expect(cardName).toBeInTheDocument() 
+expect(cardName.textContent).toBeTruthy();
+
+})
+
+// test('are names red ', async () => {
+//   const {baseElement} = render(<App/>)
+//   await waitForDomChange();
+// const Namecolor = baseElement.querySelector('.pnampad')
+
+// expect(Namecolor).toBeInTheDocument() 
+
+// expect(Namecolor).toHaveStyle({color: red})
+
+// })
+
+
+// test("namesred", async () => {
+ 
+
+//     const { getByTestId } = render(<App />);
+//     await waitForDomChange();
+//     expect(getByTestId("itdo")).toHaveStyleRule("itdo",'red');
+
+// });
